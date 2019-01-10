228d10302bb46a18ce84791e56c45bb25dc7506d
kiberleab <kiberleabeniyew@gmail.com>
2019-01-10 11:11:46 -0800

    Frist Commit

---
+++ src/main/java/com/KKHHH/eHotels/EHotelsApplication.java
@@ -0,0 +1,14 @@
+package com.KKHHH.eHotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHotelsApplication.class, args);
+	}
+
+}
+
